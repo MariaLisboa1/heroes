03dbe6fcc7bfaed849e479174ad268b4abbaee03
Nazaré Lisboa <zaza.naza95@gmail.com>
2020-05-10 00:53:45 -0300

    stylized home and list of heroes with fake data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import _ from 'lodash';
 
-// import { Container } from './styles';
+import captain from '../../assets/images/captainAmerica.jpg';
 
-function Home() {
-  return <h1>Home</h1>;
+import { Container, CardGroup, Form, SubmitButton, Card } from './styles';
+
+export default class Home extends Component {
+  state = {
+    heroes: [],
+    searchHero: [],
+  };
+
+  componentDidMount() {
+    const listHeroes = [
+      {
+        id: 1,
+        name: 'maria',
+        description:
+          'É um super-herói de histórias em quadrinhos americanos publicado pela Marvel Comics.',
+      },
+      {
+        id: 2,
+        name: 'nazare',
+        description:
+          'É um super-herói de histórias em quadrinhos americanos publicado pela Marvel Comics.',
+      },
+      {
+        id: 3,
+        name: 'capitao',
+        description:
+          'É um super-herói de histórias em quadrinhos americanos publicado pela Marvel Comics.',
+      },
+    ];
+
+    this.setState({ heroes: listHeroes, searchHero: listHeroes });
+  }
+
+  search = (event) => {
+    event.persist();
+
+    if (!this.debouncedFn) {
+      this.debouncedFn = _.debounce(() => {
+        const searchString = event.target.value;
+        const { heroes } = this.state;
+
+        const searchHero = heroes.filter((hero) =>
+          hero.name.includes(searchString)
+        );
+        this.setState({ searchHero });
+      }, 300);
+    }
+    this.debouncedFn();
+  };
+
+  render() {
+    const { searchHero } = this.state;
+    return (
+      <Container>
+        <h1>HEROES</h1>
+
+        <Form onChange={this.search}>
+          <input type="text" placeholder="Hero" />
+
+          <SubmitButton>
+            <FaSearch color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+
+        <CardGroup>
+          {searchHero.map((hero) => (
+            <Card key={hero.id}>
+              <img src={captain} alt="Capitão America" />
+              <div>
+                <h3>{hero.name}</h3>
+
+                <p>{hero.description}</p>
+              </div>
+            </Card>
+          ))}
+        </CardGroup>
+      </Container>
+    );
+  }
 }
-
-export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,103 @@
+import styled, { keyframes, css } from 'styled-components';
+
+export const Container = styled.div`
+  padding: 20px;
+
+  h1 {
+    text-align: center;
+    color: #202020;
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+
+  input {
+    flex: 1;
+    border: ${(props) =>
+      props.notFoundUser ? '1px solid red' : '1px solid #eee'};
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const SubmitButton = styled.button.attrs((props) => ({
+  type: 'submit',
+  disabled: props.loading,
+}))`
+  background: #202020;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  ${(props) =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `}
+`;
+
+export const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 191px;
+  height: 354px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  margin: 40px 20px;
+  background: #151515;
+  border-radius: 5px;
+  cursor: pointer;
+
+  img {
+    width: 191px;
+    height: 203px;
+    border-bottom: 2px solid red;
+  }
+
+  div {
+    h3 {
+      text-transform: uppercase;
+      margin: 10px 5px;
+    }
+
+    P {
+      padding: 35px 10px;
+      font-size: 12px;
+      color: #bbb;
+      font-weight: bold;
+    }
+  }
+`;
+
+export const CardGroup = styled.div`
+  display: flex;
+  color: #fff;
+`;
