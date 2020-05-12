513015787dd017c2cb0020fd9da29fe45167a3f5
Nazaré Lisboa <zaza.naza95@gmail.com>
2020-05-12 15:24:51 -0300

    refactoring in redux

--- src/store/modules/hero/reducer.js
+++ src/store/modules/hero/reducer.js
@@ -3,8 +3,6 @@ import { toast } from 'react-toastify';
 
 export default function hero(state = [], action) {
   switch (action.type) {
-    case '@hero/ALL_SUCCESS':
-      return action.hero;
     case '@hero/BY_ID_SUCCESS':
       return [action.hero];
     case '@hero/REMOVE_SERIE':
